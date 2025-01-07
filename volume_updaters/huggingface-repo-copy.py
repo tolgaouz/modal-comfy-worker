b8f8650c1d36bdf943a3e376b552fddc82339625
Tolga <tolgaouzz@gmail.com>
2025-01-07 17:58:49 +0300

    feat: add better volume update examples and use hf_transfer

--- volume_updaters/huggingface-repo-copy.py
+++ volume_updaters/huggingface-repo-copy.py
@@ -1,9 +1,21 @@
+"""
+This is a modal function that downloads a snapshot of a huggingface repo and copies it to a volume.
+
+This is useful when you have a huggingface repo shaped exactly like ComfyUI's /models directory.
+
+You can use this function to download the repo and copy it to a volume, then use the volume in your ComfyUI container as the /models directory.
+"""
+
 from modal import Secret, Image, App, Volume
 import os
 from huggingface_hub import snapshot_download
 from ..workflow import APP_NAME, VOLUME_NAME
 
-image = Image.debian_slim().pip_install("requests", "huggingface-hub")
+image = (
+    Image.debian_slim()
+    .pip_install("huggingface_hub[hf_transfer]==0.26.2")
+    .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
+)
 
 app = App(f"{APP_NAME}-update-volume")
 volume = Volume.from_name(VOLUME_NAME)
