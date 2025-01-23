965feadea739d4d9604f18c7d2fbc2ecf4b8ea78
Tolga <tolgaouzz@gmail.com>
2025-01-24 01:36:16 +0300

    refactor: remove example clauses

--- src/volume_updaters/individual-hf-models.py
+++ src/volume_updaters/individual-hf-models.py
@@ -4,29 +4,18 @@ This is a modal function that downloads a list of models from huggingface and co
 The volume is then used as the /models directory in your ComfyUI container.
 """
 
-from modal import Secret, Image, App, Volume
-from ..workflow import APP_NAME, VOLUME_NAME
-
-image = (
-    Image.debian_slim()
-    .pip_install("huggingface_hub[hf_transfer]==0.26.2")
-    .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
-)
+from modal import Secret, App, Volume
+from ..workflow import APP_NAME, VOLUME_NAME, image
 
 app = App(f"{APP_NAME}-update-volume")
-volume = Volume.from_name(VOLUME_NAME)
+volume = Volume.from_name(VOLUME_NAME, create_if_missing=True)
 
 models_to_download = [
     # format is (huggingface repo_id, the model filename, comfyui models subdirectory we want to save the model in)
     (
-        "black-forest-labs/FLUX.1-schnell",
-        "vae.safetensors",
-        "vae",
-    ),
-    (
-        "black-forest-labs/FLUX.1-schnell",
-        "flux1-schnell.safetensors",
-        "unet",
+        "Comfy-Org/flux1-dev",
+        "flux1-dev-fp8.safetensors",
+        "checkpoints",
     ),
     (
         "comfyanonymous/flux_text_encoders",
@@ -41,7 +30,6 @@ models_to_download = [
     volumes={"/volume": volume},
     image=image,
     timeout=3600,
-    secrets=[Secret.from_name("huggingface-secret")],
 )
 def hf_download(repo_id: str, filename: str, model_type: str):
     from huggingface_hub import hf_hub_download
